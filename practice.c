f8f18f6f7f0629103084b197dd91b2b1ac2244ee
Talha-on-Git <talhayousuf107@gmail.com>
2025-11-13 19:42:46 +0500

    Made Dashboard UI

--- practice.c
+++ practice.c
@@ -1,34 +1,62 @@
 #include <raylib.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     const int screenWidth = 1000;
     const int screenHeight = 600;
-
-    InitWindow(screenWidth, screenHeight, "Hello Raylib");
+    InitWindow(screenWidth, screenHeight, "Point Management System");
 
     SetTargetFPS(60);
 
-    float boxWidth = 300;
-    float boxHeight = 150;
-    float boxX = screenWidth / 2.0f - boxWidth / 2.0f;
-    float boxY = screenHeight / 2.0f - boxHeight / 2.0f;
+    Font font = GetFontDefault();
+    Color cardColor = (Color){255, 255, 255, 230};
+    Color bgColor = (Color){0, 180, 255, 255};
+    Color buttonColor = (Color){255, 255, 255, 255};
+    Color buttonHover = (Color){220, 220, 255, 255};
+    Color textColor = (Color){0, 80, 180, 255};
 
-    // Step 5: Main loop
+    Rectangle card = (Rectangle){screenWidth / 2.0f - 150, 100, 300, 420};
+    Rectangle buttons[4];
+    for (int i = 0; i < 4; i++)
+    {
+        buttons[i] = (Rectangle){card.x + 40, card.y + 200 + (i * 50), 220, 35};
+    }
+
+    const char *labels[4] = {
+        "View Routes",
+        "Check Seat Availability",
+        "Book a Seat",
+        "View Time Schedule"};
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(BLUE);
+        ClearBackground(bgColor);
 
-        DrawRectangle((int)boxX, (int)boxY, (int)boxWidth, (int)boxHeight, RAYWHITE);
+        DrawRectangleRounded(card, 0.1f, 10, cardColor);
+        DrawRectangleRoundedLines(card, 0.1f, 10, 2, Fade(BLUE, 0.3f));
 
-        const char *message = "Hello Raylib";
-        int fontSize = 30;
-        int textWidth = MeasureText(message, fontSize);
-        int textX = screenWidth / 2 - textWidth / 2;
-        int textY = screenHeight / 2 - fontSize / 2;
+        DrawText("Student Dashboard", (int)(card.x + 40), (int)(card.y + 20), 20, textColor);
 
-        DrawText(message, textX, textY, fontSize, BLUE);
+        int textX = (int)(card.x + 20);
+        int textY = (int)(card.y + 60);
+        DrawText("Name: Ali Ahmed", textX, textY, 16, BLACK);
+        DrawText("Batch: BSCS-23", textX, textY + 25, 16, BLACK);
+        DrawText("Roll No: 23K-1234", textX, textY + 50, 16, BLACK);
+        DrawText("Point Bus: Bus 5", textX, textY + 75, 16, BLACK);
+        DrawText("Route: Green Line", textX, textY + 100, 16, BLACK);
+
+        for (int i = 0; i < 4; i++)
+        {
+            bool hovered = CheckCollisionPointRec(GetMousePosition(), buttons[i]);
+            DrawRectangleRounded(buttons[i], 0.3f, 10, hovered ? buttonHover : buttonColor);
+            DrawRectangleRoundedLines(buttons[i], 0.3f, 10, 1, Fade(BLUE, 0.3f));
+
+            int textWidth = MeasureText(labels[i], 14);
+            DrawText(labels[i], (int)(buttons[i].x + (buttons[i].width - textWidth) / 2), (int)(buttons[i].y + 10),14, textColor);
+        }
 
         EndDrawing();
     }
